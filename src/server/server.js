84c07974e4900a4cb20a4f584636a6600d4a50a3
Irfan Sobbah M <irfansobbahm@gmail.com>
2025-12-12 09:34:41 +0700

    port

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,25 @@
 import { serve } from '@hono/node-server'
 import app from './index.js'
 
-const port = process.env.PORT || 3000
-console.log(`🚀 Server is running on http://localhost:${port}`)
+// Vercel environment variable otomatis ada
+const isVercel = !!process.env.VERCEL;
 
-serve({
-  fetch: app.fetch,
-  port
-})
+if (!isVercel) {
+  // =================================
+  // LOCAL DEVELOPMENT
+  // =================================
+  const port = process.env.PORT || 3000
+  console.log(`🚀 Local server is running on http://localhost:${port}`)
+  
+  serve({
+    fetch: app.fetch,
+    port
+  })
+} else {
+  // =================================
+  // VERCEL DEPLOYMENT
+  // =================================
+  console.log('✅ Running on Vercel serverless, no need to listen port')
+}
+
+export default app
